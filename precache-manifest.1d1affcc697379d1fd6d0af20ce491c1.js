950a9f4b1c624a8e07b1c7c28a1449a68f779b96
codefire53 <krisnaihsani@gmail.com>
2020-07-30 21:09:41 +0700

    Updates

---
+++ precache-manifest.1d1affcc697379d1fd6d0af20ce491c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3566b54e6cce7c55196fa66a1578af7",
+    "url": "/codefire53-github-io/index.html"
+  },
+  {
+    "revision": "fe4eef5cefb76336b72e",
+    "url": "/codefire53-github-io/static/css/main.e1d1a0e4.chunk.css"
+  },
+  {
+    "revision": "1e3832c9d27900ec9513",
+    "url": "/codefire53-github-io/static/js/2.5e1e1b43.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/codefire53-github-io/static/js/2.5e1e1b43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe4eef5cefb76336b72e",
+    "url": "/codefire53-github-io/static/js/main.d2c544d0.chunk.js"
+  },
+  {
+    "revision": "c578c50d21cbc4d7ce13",
+    "url": "/codefire53-github-io/static/js/runtime-main.8d19bca3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/codefire53-github-io/static/media/logo.5d5d9eef.svg"
+  }
+]);
